f5f19976e8947485066be20da8fd6276eba487b0
Abram Hindle <abram.hindle@ualberta.ca>
2016-07-29 16:10:12 -0600

    Adding states

--- scripts/processConfs.pl
+++ scripts/processConfs.pl
@@ -22,21 +22,34 @@ my %fields = (
               DOI=>0,
               Refereed=>1,
               Authors=>1,
+              URL=>0,
               Region => 0,
               Note => 0,
               dmgKey => 0,
              );
 
 my %subregion = (
-                 "Ontario" => "Canada",
                  "British Columbia" => "Canada",
+                 "Alberta" => "Canada",
+                 "Saskatchewan" => "Canada",
+                 "Manitoba" => "Canada",
+                 "Ontario" => "Canada",
                  "Quebec" => "Canada",
+                 "Nova Scotia" => "Canada",
+                 "Prince Edward Island" => "Canada",
+                 "New Brunswick" => "Canada",
+                 "Newfoundland" => "Canada",
                  "California" => "United States",
                  "Virginia" => "United States",
                  "Nevada" => "United States",
                  "Hawaii" => "United States",
                  "New Mexico" => "United States",
-                 );
+                );
+my @united_states = ("Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming");
+for my $state (@united_states) {
+    $subregion{$state} = "United States";
+}
+
 
 my %countries = (
                  "Belgium" => 1,
