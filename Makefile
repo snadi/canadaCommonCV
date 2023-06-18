5829af3ca35fa6d17a455c816cca8da132409155
Sarah Nadi <nadi@ualberta.ca>
2023-06-18 15:08:38 +0300

    Add complete pipeline

--- Makefile
+++ Makefile
@@ -1,30 +1,39 @@
 INCS=scripts/common.pm
+INPUT=input/
+OUTPUT=output/
 
-default: creatdir confs journals
+default: $(OUTPUT) processedfiles confs journals
 
-creatdir:
-	rm -rf output/*
-	mkdir -p output
+$(OUTPUT):
+	mkdir -p $@
 
-journals: journals.xml journals.bib
+processedfiles: $(OUTPUT)confs.txt $(OUTPUT)journals.txt
 
-confs: confs.xml confs.bib
+journals: $(OUTPUT)journals.xml $(OUTPUT)journals.bib
 
-confs.xml: scripts/processConfs.pl $(INCS) input/confs.txt
-	perl $< input/confs.txt > tmp.tmp && mv tmp.tmp  output/$@
+confs: $(OUTPUT)confs.xml $(OUTPUT)confs.bib
 
-journals.xml: scripts/processJournals.pl $(INCS) input/journals.txt
-	perl $< input/journals.txt > tmp.tmp && mv tmp.tmp  output/$@
+$(OUTPUT)pubs.json: input/pubs.bib bib_to_json.py
+	python bib_to_json.py --bibfile input/pubs.bib > $@
 
-confs.bib: scripts/txtToLatexConfs.pl $(INCS)  input/confs.txt
-	perl $< input/confs.txt > tmp.tmp && mv tmp.tmp  output/$@
+$(OUTPUT)confs.txt $(OUTPUT)journals.txt: $(OUTPUT)pubs.json json_to_dmg.py
+	python json_to_dmg.py --jsonfile $(OUTPUT)pubs.json
 
-journals.bib:  scripts/txtToLatexJournals.pl $(INCS) input/journals.txt
-	perl $< input/journals.txt > tmp.tmp && mv tmp.tmp  output/$@
+$(OUTPUT)confs.xml: scripts/processConfs.pl $(INCS) processedfiles
+	perl $< $(OUTPUT)confs.txt > tmp.tmp && mv tmp.tmp  $@
+
+$(OUTPUT)journals.xml: scripts/processJournals.pl $(INCS) processedfiles
+	perl $< $(OUTPUT)journals.txt > tmp.tmp && mv tmp.tmp $@
+
+$(OUTPUT)confs.bib: scripts/txtToLatexConfs.pl $(INCS) processedfiles
+	perl $< $(OUTPUT)confs.txt > tmp.tmp && mv tmp.tmp $@
+
+$(OUTPUT)journals.bib:  scripts/txtToLatexJournals.pl $(INCS) processedfiles
+	perl $< $(OUTPUT)journals.txt > tmp.tmp && mv tmp.tmp $@
 
 clean:
-	rm -f output/journals.bib output/confs.bib output/journals.xml output/confs.xml
+	rm -f output/*
 
-bibs:	output/confs.bib output/journals.bib
+bibs:	$(OUTPUT)confs.bib $(OUTPUT)journals.bib
 
-xmls:   output/confs.xml output/journals.xml
+xmls:   $(OUTPUT)confs.xml $(OUTPUT)journals.xml
---
+++ bib_to_json.py
@@ -0,0 +1,88 @@
+from pybtex.database import parse_file
+import argparse
+import json
+
+# fill with your own HQP names
+hqp = ["Amann, Sven", 
+            "Proksch, Sebastian", 
+            "Cergani, Ervina",
+            "Masri, Samer Al", 
+            "Mahmoudi, Mehran", 
+            "de la Mora, Fernando Lopez", 
+            "Radu, Aida", 
+            "Kareshk, Moein Owhadi", 
+            "Bhuiyan, Nazim Uddin", 
+            "Jha, Ajay Kumar", 
+            "El-Hajj, Rehab", 
+            "Kamath, Ram", 
+            "Tang, Henry", 
+            "Nuryyev, Batyr", 
+            "Galappaththi, Akalanka", 
+            "Nguyen, Nhan", 
+            "Gulami, Mansur", 
+            "Ellis, Max", 
+            "Islam, Mohayeminul"]
+
+students = ["Lin, Changyuan",
+            "Soni, Abhishek",
+            "Soares, Larissa Rocha"]
+
+def process_authors(authors, tex=False):
+    author_list = []
+    for author in authors:
+        author = str(author)
+        if tex and author in hqp:
+            author_list.append("\\HQP{%s}" % author)
+        elif tex and author in students:
+            author_list.append("\\student{%s}" % author)
+        else:
+            author_list.append(author)
+    return ' and '.join(author_list)
+
+def process_acceptance(addendum):
+    # input format is Acceptance Ratio: 12/30 = 40\%
+    # return "12/30 = 40%"
+    return addendum.split(":")[1].strip().replace("\%","%")
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--bibfile')
+    args = parser.parse_args()
+
+    bib_data = parse_file(args.bibfile)
+
+    jsondata = {}
+    jsondata['journals'] = []
+    jsondata['conferences'] = []
+
+    for entry in bib_data.entries.keys():
+        paper = bib_data.entries[entry]
+        jsonentry = {}
+        
+        jsonentry['authors_tex'] = process_authors(paper.persons['author'], tex=True)
+        jsonentry['authors'] = process_authors(paper.persons['author'])
+
+        #keys with same name in bib and json format
+        for key in ['title', 'year', 'publisher', 'doi', 'city', 'country', 'volume']:
+            if key in paper.fields:
+                jsonentry[key] = paper.fields[key]
+                
+        if 'pages' in paper.fields:
+            jsonentry['pagerange'] = paper.fields['pages']
+
+        if 'addendum' in paper.fields:
+            jsonentry['rate'] = process_acceptance(paper.fields['addendum'])
+
+        jsonentry['PublishingStatus'] = 'Published' # assumption is entries appearing in bib file are published
+
+        if paper.type == 'article': # journal specific values
+            jsonentry['venue'] = paper.fields['journal']
+            jsondata['journals'].append(jsonentry)
+        elif paper.type == 'inproceedings': # conference specific values
+            jsonentry['venue'] = paper.fields['booktitle']
+            jsondata['conferences'].append(jsonentry)
+
+    print(json.dumps(jsondata, indent=4))
+
+if __name__ == "__main__":
+    main()
