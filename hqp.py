cb2a3d877cec2fdecba570b28b2dc4e34ddac2ee
Sarah Nadi <nadi@ualberta.ca>
2023-06-27 06:52:06 +0300

    Use sample names

--- hqp.py
+++ hqp.py
@@ -1,25 +1,6 @@
 
 # fill with your own HQP names
-hqp = ["Amann, Sven", 
-            "Proksch, Sebastian", 
-            "Cergani, Ervina",
-            "Masri, Samer Al", 
-            "Mahmoudi, Mehran", 
-            "de la Mora, Fernando Lopez", 
-            "Radu, Aida", 
-            "Kareshk, Moein Owhadi", 
-            "Bhuiyan, Nazim Uddin", 
-            "Jha, Ajay Kumar", 
-            "El-Hajj, Rehab", 
-            "Kamath, Ram", 
-            "Tang, Henry", 
-            "Nuryyev, Batyr", 
-            "Galappaththi, Akalanka", 
-            "Nguyen, Nhan", 
-            "Gulami, Mansur", 
-            "Ellis, Max", 
-            "Islam, Mohayeminul"]
+hqp = ["Duck, Donald", 
+            "Goofey, Goofey", ]
 
-students = ["Lin, Changyuan",
-            "Soni, Abhishek",
-            "Soares, Larissa Rocha"]
+students = ["Jerry, Tom", "Mouse, Mickey"]
