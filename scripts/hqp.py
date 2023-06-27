f146908fb7dfea3239c4eea0b95b80a47e2d5b15
Sarah Nadi <nadi@ualberta.ca>
2023-06-27 11:19:04 +0300

    Fix hqp name

--- scripts/hqp.py
+++ scripts/hqp.py
@@ -1,12 +1,12 @@
 
 # fill with your own HQP names
-hqp = ["Nguyen Quang Do, List",
+hqp = ["Do, Lisa Nguyen Quang",
        "Spath, Johannes",
        "Ochoa, Erick",
        "Tiganov, Daniil",
        "Cho, Jeff",
        "Bangash, Abdul Ali",
-       "Newburry, Kristen",
+       "Newbury, Kristen",
        "Xia, Cijie"]
 
 students = ["Chikin, Artem", "Nashaat, Mona", "Sahar, Hareem", "LLlyod, Taylor", "Gulami, Mansur"]
